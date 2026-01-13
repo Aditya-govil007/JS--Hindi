f04e5ca04d3841d29d8a15825f2ca317ce7c39b1
Aditya-govil007 <adityagovil2004@gmail.com>
2026-01-13 15:04:35 +0000

    Variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountID=144553
+let accountEmail="aditya@gmail.com"
+var accountPassword="12345"
+accountCity="Lucknow"
+let accountState
+
+//accountID=2
+// we can use const and let not var
+
+accountEmail="agag@gmail.com"
+accountPassword="212121"
+accountCity="banglore"
+
+console.log(accountID);
+console.log(accountEmail);
+
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
