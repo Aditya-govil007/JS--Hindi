fa9eb5dbaed4d3da6a439ad543ed5b729c100bb9
Aditya-govil007 <adityagovil2004@gmail.com>
2026-01-30 12:01:07 +0000

    conversion of datatypes

--- 01_Basics/03_CoversionOperation.js
+++ 01_Basics/03_CoversionOperation.js
@@ -6,3 +6,17 @@ console.log(typeof (score))
 let valueInnumber= Number(score)
 console.log(typeof (valueInnumber))
 console.log(valueInnumber)
+
+
+let isloggedIn= "aditya"
+
+let booleanisloggedIn=Boolean(isloggedIn)
+console.log(booleanisloggedIn)
+
+
+let sum=0
+
+let getstring=String(sum)
+
+console.log(getstring)
+console.log(typeof getstring)
