c38a76f38f370e7f0f165db0189f5425bfb660a7
Aditya-govil007 <adityagovil2004@gmail.com>
2026-01-31 09:58:52 +0000

    Conversion Dataytype

--- 01_Basics/03_CoversionOperation.js
+++ 01_Basics/03_CoversionOperation.js
@@ -20,3 +20,17 @@ let getstring=String(sum)
 
 console.log(getstring)
 console.log(typeof getstring)
+
+let str1= "hello"
+let str2 =" aditya"
+
+let str3= str1+str2 
+console.log(str3)  //this gives op as hello aditya
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+"2")
+//above all give op as 12
+
+console.log("1"+"2"+2)//this give O/P as 122
+console.log(1+2+"2") //this gies O/P as 32 bcoz the first value is recognised as number then followed by string which gets printed as it is
