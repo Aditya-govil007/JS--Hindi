e848a8819019092b18caf4a6edc66491e190fb89
Aditya-govil007 <adityagovil2004@gmail.com>
2026-01-31 10:15:53 +0000

    Comparsion

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,28 @@
+//console.log(2>1)
+// this gives O/P as Boolean value either true  or false
+
+//console.log("01">=1)
+//console.log(01<=2) 
+// we can compare different dataytpe as well we can se above
+
+//Comparison Operators {<,>,>=,<=,==}
+
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null<0)
+// console.log(null<=0)
+// console.log(null>=0)
+
+//null is treated as 0 in js in for >= or <= not for ==;
+
+//console.log(undefined==0)
+// undefined always gives false on comparsion remember that
+
+
+
+
+
+console.log('2'===2)
+// here it also check the type of datatype so it gives O/P as false
+
+// the comparsion here creates ambiguity so avoid these
