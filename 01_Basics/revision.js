ea615751f0091f128cab7cf81df38be153d8cb4d
Aditya-govil007 <adityagovil2004@gmail.com>
2026-01-31 10:47:44 +0000

    summary till datatype

---
+++ 01_Basics/revision.js
@@ -0,0 +1,25 @@
+// Primitive
+
+// Datatype - number,string,null, undefined, symbol, BigInt
+
+
+
+
+const score = 100
+console.log(typeof score)
+
+
+//Non Primitive or Reference
+
+
+//Array, Objects{must},Function
+
+const heroes=['hulku','ion man','thor'];
+let myobj={
+    name: 'aditya',
+    age: 22
+}
+const myfunction = function (){
+    console.log("hello world")
+}
+console.log(myfunction)
