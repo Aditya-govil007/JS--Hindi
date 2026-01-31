d4e07ed0d55c9254fb55dcab94cd06d810a94b23
Aditya-govil007 <adityagovil2004@gmail.com>
2026-01-31 10:06:47 +0000

    conversion

--- 01_Basics/03_CoversionOperation.js
+++ 01_Basics/03_CoversionOperation.js
@@ -1,40 +1,57 @@
-let score = undefined
+// let score = undefined
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
-let valueInnumber= Number(score)
-console.log(typeof (valueInnumber))
-console.log(valueInnumber)
-
-
-let isloggedIn= "aditya"
-
-let booleanisloggedIn=Boolean(isloggedIn)
-console.log(booleanisloggedIn)
+// let valueInnumber= Number(score)
+// console.log(typeof (valueInnumber))
+// console.log(valueInnumber)
 
 
-let sum=0
+// let isloggedIn= "aditya"
 
-let getstring=String(sum)
-
-console.log(getstring)
-console.log(typeof getstring)
-
-let str1= "hello"
-let str2 =" aditya"
-
-let str3= str1+str2 
-console.log(str3)  //this gives op as hello aditya
-
-console.log("1"+2)
-console.log(1+"2")
-console.log("1"+"2")
-//above all give op as 12
-
-console.log("1"+"2"+2)//this give O/P as 122
-console.log(1+2+"2") //this gies O/P as 32 bcoz the first value is recognised as number then followed by string which gets printed as it is
+// let booleanisloggedIn=Boolean(isloggedIn)
+// console.log(booleanisloggedIn)
 
 
-console.log((3+4)*5%3) 
-// always use parenthesis for complex code conversion 
+// let sum=0
+
+// let getstring=String(sum)
+
+// console.log(getstring)
+// console.log(typeof getstring)
+
+// let str1= "hello"
+// let str2 =" aditya"
+
+// let str3= str1+str2 
+// console.log(str3)  //this gives op as hello aditya
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+"2")
+// //above all give op as 12
+
+// console.log("1"+"2"+2)//this give O/P as 122
+// console.log(1+2+"2") //this gies O/P as 32 bcoz the first value is recognised as number then followed by string which gets printed as it is
+
+
+// console.log((3+4)*5%3) 
+// // always use parenthesis for complex code conversion 
+
+// console.log(+true)
+
+// console.log(+"")
+
+// let num1,num2,num3
+
+// num1=num2=num3 = 2+2
+
+// console.log(num1)
+
+let gameCounter=100
+console.log(gameCounter)
+//gameCounter++;
+//console.log(gameCounter)
+++gameCounter;
+console.log(gameCounter)
