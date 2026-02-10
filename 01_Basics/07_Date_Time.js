7ca22e33720428f1ab84dcbcab118a7bd8ac8409
Aditya Govil <adityagovil2004.com>
2026-02-10 22:07:28 +0530

    date and time

---
+++ 01_Basics/07_Date_Time.js
@@ -0,0 +1,18 @@
+//Date 
+
+let mydate=new Date()
+
+//console.log(mydate);
+// console.log(mydate.toString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toTimeString());
+// console.log(mydate.toJSON());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toTimeString());
+// console.log(mydate.toUTCString());
+console.log(typeof ( mydate))
+
+
+
+
