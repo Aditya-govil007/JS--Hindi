7d9a5dffce931693912e1c38af11830a27d98a7c
Aditya Govil <adityagovil2004.com>
2026-02-10 21:23:31 +0530

    Math and Num explanation

---
+++ 01_Basics/06_Math_and_Num.js
@@ -0,0 +1,41 @@
+// ------------NUMBERS----------------------
+
+// const score=400
+
+// console.log(score);
+
+// const balance = new Number(100)//this variable will only be number
+
+// console.log(balance);
+// console.log(balance.toString());//to convert it to string datatype
+// console.log(balance.toFixed(3));//for decimal places
+
+// console.log(typeof (balance));
+
+// const newnumber= new Number(69.696969)
+// console.log(newnumber.toPrecision(3));
+
+
+
+//-------------------------------MATHS---------------------------------------
+
+console.log(Math);
+console.log(Math.abs(-44));// abs -> converts negative to posititve
+console.log(Math.round(3.69));// round-> round off krdeta hai.
+console.log(Math.ceil(3.69));// ceil-> round off krdeta hai top value pr.(badi value pr)
+console.log(Math.floor(3.69));// floor-> round off krdeta hai low value pr.(sabse choti value)
+
+console.log(Math.min(4,3,6,8,9));
+console.log(Math.max(4,3,6,8,9));
+
+console.log(Math.random());//generates random digits b/w 0-1
+
+const min= 20
+const max=30
+
+console.log(Math.floor(Math.random() * (max - min +1)+min))
+
+
+
+
+
