2c8fa98d01a068f68476532a69e0a36b596da14d
Aditya Govil <adityagovil2004.com>
2026-02-11 19:05:21 +0530

    Date and Time learned

--- 01_Basics/07_Date_Time.js
+++ 01_Basics/07_Date_Time.js
@@ -1,6 +1,6 @@
 //Date 
 
-let mydate=new Date()
+//let mydate=new Date()
 
 //console.log(mydate);
 // console.log(mydate.toString());
@@ -11,8 +11,16 @@ let mydate=new Date()
 // console.log(mydate.toLocaleString());
 // console.log(mydate.toTimeString());
 // console.log(mydate.toUTCString());
-console.log(typeof ( mydate))
+//console.log(typeof ( mydate))
+
+// let createdate= new Date(23,0,2023);
+// console.log(createdate.toLocaleDateString());
+// console.log(createdate.toDateString());
 
 
+let newdate= new Date()
+console.log(newdate);
+console.log(newdate.getMonth()+1);
+console.log(newdate.getDate());
 
-
+$ {newdate.getDate()}
