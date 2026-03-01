4461f473a2ce9a1c8b916f30e9cc90b213433f15
Aditya Govil <adityagovil2004.com>
2026-03-01 17:14:02 +0530

    Starting of Scopes

---
+++ 03_Basics/02_Scopes.js
@@ -0,0 +1,21 @@
+// -------------SCOPE->denoted by this =>"{}"------------------
+// var c=3000
+// this is global scope 
+let a=300
+
+
+// this is local scope
+if(true){
+    let a=10
+    const b =20
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+
+console.log(a);
+// console.log(b);
+// console.log(c);
