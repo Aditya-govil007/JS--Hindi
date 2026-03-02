635d07a47100cac703d867a5f6c4f9576625c1d3
Aditya Govil <adityagovil2004@gmail.com>
2026-03-02 11:37:07 +0530

    Scopes learned

--- 03_Basics/02_Scopes.js
+++ 03_Basics/02_Scopes.js
@@ -10,15 +10,54 @@ if(true){
     const b =20
 }
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
     
+// }
+
+
+//console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username="aditya"
+
+    function two(){
+        const website="youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+
+    two()
+    
+}
+// one()
+
+if(true){
+    const username="aditya"
+    if(username==="aditya"){
+        const website=" youtube"
+        // console.log(username+website);
+        
+    }
+    // console.log(website);
+    
+
+    
+}
+// console.log(username);
+
+// ------------Interesting-------------
+
+console.log(addone(5))
+function addone(num){
+    return num+1
 }
 
 
-console.log(a);
-
-
-
-// console.log(b);
-// console.log(c);
+const addtwo= function(num){
+    return num+2
+}
+addtwo(5)
