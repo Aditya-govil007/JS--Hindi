46391b4bf433ec9a7d7057418fb4c2bc8a5a4138
Aditya Govil <adityagovil2004@gmail.com>
2026-03-03 17:14:56 +0530

    Started learning Loops

---
+++ 04_Control_Flow/one.js
@@ -0,0 +1,31 @@
+//------------------ if statement-------------
+
+// if(true){
+     // code execute otherwise if false no code execution
+// }
+
+// if(2=="3"){
+//     console.log("executed");
+    
+// const and let is to space or block spoce but var is global it can be accesed anywhere
+
+
+const score=200
+if(score>100){
+    let power="fly"
+    // console.log(`User power : ${power}`);
+}
+
+// console.log(`User power : ${power}`)
+
+const balance = 1000
+
+// if(balance>500) //console.log("true");
+
+const userloggedIn=true;
+const debitcard=true;
+
+if(userloggedIn && debitcard){
+    console.log("Allow to buy");
+    
+}
