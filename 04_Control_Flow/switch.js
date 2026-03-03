cabc69f638c75135db41fe6e378cb94fa14636a7
Aditya Govil <adityagovil2004@gmail.com>
2026-03-03 21:50:21 +0530

    started learning switch case

---
+++ 04_Control_Flow/switch.js
@@ -0,0 +1,28 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month){
+    case 1:
+        console.log("month is -> January");
+        break;
+    case 2:
+        console.log("month is -> February");
+        break;
+    case 3:
+        console.log("month is -> March");
+        break;
+    case 4:
+        console.log("month is -> April");
+        break;
+
+    default:
+        break;
+}
