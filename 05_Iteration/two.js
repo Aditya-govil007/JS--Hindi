25302095e111c85a2f72c660f7426c94b584938f
Aditya Govil <adityagovil2004@gmail.com>
2026-03-06 11:56:08 +0530

    while and do while loop

---
+++ 05_Iteration/two.js
@@ -0,0 +1,23 @@
+// ------------------While loop------------------------
+let index =0
+while (index<=10) {
+    // console.log(`Value of indes is : ${index}`);
+    index++
+    
+    
+}
+
+let myarr=['flash','superman','batman']
+let arr=0
+while(arr<myarr.length){
+    // console.log(`Value is ${myarr[arr]}`);
+    arr++
+    
+}
+
+// --------------Do while loop-----------------------
+let score=1
+do{
+    console.log(`Score is ${score}`);
+    score++
+}while(score<=10);
