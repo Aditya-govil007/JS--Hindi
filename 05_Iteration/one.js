12726d592abd265a2cc74070b218c9489c3380fb
Aditya Govil <adityagovil2004@gmail.com>
2026-03-06 11:45:03 +0530

    Iteration learned

--- 05_Iteration/one.js
+++ 05_Iteration/one.js
@@ -1,9 +1,34 @@
 // For loop
 
 
-for (let index = 0; index <10; index++) {
-    const element = index;
-    console.log(element);
+// for (let index = 0; index <10; index++) {
+//     const element = index;
+//     console.log(element);
+    
+    
+// }
+
+for (let i = 1; i <=10; i++) {
+    // console.log(`Outer Loop : ${i}`);
+    
+    for (let j = 1; j < 10; j++) {
+        // console.log(i+ '*' +j+'='+i*j);
+        
+        
+    }
+    
+}
+
+//  Break and Continue condition ->
+
+for (let index = 1; index <=20; index++) {
+    // const element = array[index];
+    if(index==5){
+        console.log("5 detected");
+        continue;
+        
+    }
+    console.log(`Values of i is : ${index}`);
     
     
 }
