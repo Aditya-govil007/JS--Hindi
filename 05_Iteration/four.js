764397794db05e6056465203721f97368e145fd5
Aditya Govil <adityagovil2004@gmail.com>
2026-03-07 15:07:53 +0530

    for in loop

---
+++ 05_Iteration/four.js
@@ -0,0 +1,21 @@
+const myobject={
+    js : 'javascript',
+    cpp: 'c++',
+    py : 'python',
+    swift:'swift by apple'
+}
+
+// -----------------for in loop-------------
+for (const key in myobject) {
+    // console.log(`${key} shortcut is for ${myobject[key]}`);
+    
+
+}
+
+const progrming = ['js','py','cpp']
+for (const key in progrming) {
+    console.log(`${progrming[key]}`);
+    
+    
+    
+}
