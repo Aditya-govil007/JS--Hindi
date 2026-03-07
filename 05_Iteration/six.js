be1614edee723d89b568c5cde7a5d9d967c42681
Aditya Govil <adityagovil2004@gmail.com>
2026-03-07 19:31:51 +0530

    kearned more about for each

---
+++ 05_Iteration/six.js
@@ -0,0 +1,22 @@
+// const coding=['js','python','java','c++','c']
+
+// const values=coding.forEach((item)=>{
+//     // console.log(item);
+//     return item
+    
+// })
+
+const mynum=[1,2,3,4,5,6,7,8,9,10]
+
+// const newnum=mynum.filter((num)=>num>4)
+// console.log(newnum);
+// this above one we have not open the scope 
+
+// const newnum=mynum.filter( (num)=>{   <----
+//     return num>4                          
+// })
+// console.log(newnum);
+
+// and if we open the scope so for this type of situation we will use return 
+
+
